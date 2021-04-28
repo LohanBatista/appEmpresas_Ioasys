b7a928a25f9adf2fefb1d2f00010f398f1d029d1
Lohan Batista <lohan_vinicius@hotmail.com>
2021-04-28 02:34:56 -0300

    ios

---
+++ ios/Pods/Headers/Private/RNLaunchNavigator/WE_ReactNativeLogger.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-launch-navigator/ios/RNLaunchNavigator/WE_ReactNativeLogger.h
---
+++ ios/Pods/Target Support Files/RNLaunchNavigator/RNLaunchNavigator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RNLaunchNavigator : NSObject
+@end
+@implementation PodsDummy_RNLaunchNavigator
+@end
